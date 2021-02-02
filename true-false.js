9cca23cd77511cd2068a293e87d171215c3ffbf6
Towhidul Islam <towhidulrafi1999@gmail.com>
2021-02-02 15:41:45 +0600

    null vs undefined

---
+++ true-false.js
@@ -0,0 +1,13 @@
+let v= {};
+if (v){
+    console.log("TRUE")
+}else{
+    console.log("FALSE")
+}
+
+let a=0;
+if( a || a==0 ){
+    console.log("Condition True");
+}else{
+    console.log("Condition True");
+}
