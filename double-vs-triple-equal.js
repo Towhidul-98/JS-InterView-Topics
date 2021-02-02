113bbcab7fa6a3d759e347a4ea7ca6daf48b0e13
Towhidul Islam <towhidulrafi1999@gmail.com>
2021-02-02 16:01:28 +0600

    equalness

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,51 @@
+let a = 2;
+let b = "2";
+if(a == b){          /// type of two variable are not checked here
+    console.log("a and b are equal")
+}else{
+    console.log("a and b are not equal")
+}
+
+if(a === b){         /// type of two variable are  checked here
+    console.log("a and b are equal")
+}else{
+    console.log("a and b are not equal")
+}
+
+
+const num1 = 2;
+const num2 = true ;
+
+if(num1 == num2){          /// type of two variable are not checked here
+    console.log("num1 and num2 are equal")
+}else{
+    console.log("num1 and num2 are not equal")
+}
+
+
+const num3 = 1;
+const num4 = true ;
+
+if(num3 == num4){          /// type of two variable are not checked here
+    console.log("num3 and num4 are equal")
+}else{
+    console.log("num3 and num4 are not equal")
+}
+
+const num5 = 0;
+const num6 = false ;
+
+if(num5 == num6){          /// type of two variable are not checked here
+    console.log("num5 and num6 are equal")
+}else{
+    console.log("num5 and num6 are not equal")
+}
+
+const num7 = 1;
+const num8 = true ;
+
+if(num7 === num8){          /// type of two variable are not checked here
+    console.log("num7 and num8 are equal")
+}else{
+    console.log("num7 and num8 are not equal")
+}
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,30 +1,31 @@
-// let num1 ;   // Way 1 to get undefined
+let num1 ;   // Way 1 to get undefined
 
-// function add(num1 ,num2){    // Way 2 to get undefined
-//     console.log(num1+num2);
-// }
-// let result1 = add(3,4)
-// console.log(result1)
+function add(num1 ,num2){    // Way 2 to get undefined
+    console.log(num1+num2);
+}
+let result1 = add(3,4)
+console.log(result1)
 
-// function add1(num1 ,num2){    // Way 3 to get undefined
-//     console.log(num1+num2);
-//     return
-// }
-// let result2 = add1(3,4)
-// console.log(result2)
+function add1(num1 ,num2){    // Way 3 to get undefined
+    console.log(num1+num2);
+    return
+}
+let result2 = add1(3,4)
+console.log(result2)
 
 
 
-// function add2(num1 ,num2){    // Way 4 to get undefined
-//     console.log(num1,num2);  
-// }
-// console.log(add2(12))
+function add2(num1 ,num2){    // Way 4 to get undefined
+    console.log(num1,num2);  
+}
+console.log(add2(12))
 
-// const student = {     // Way 5 to get undefined
-//     studentId : 32 ,
-//     studentName : 'RAFI'
-// }
-// console.log(student.address)
+const student = {     // Way 5 to get undefined
+    studentId : 32 ,
+    studentName : 'RAFI'
+}
+console.log(student.address)
  
 let fun = undefined;  // Way 6 to get undefined
 console.log(fun)
+
